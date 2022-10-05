6d2e2355de1e07f70435f59349328aa01d45e2ea
pushparajsg <gowthamraj1912@gmail.com>
2022-10-06 02:01:47 +0530

    Login Screen design Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-// Main component Start's here 
+// Main component Start's here
 
-import {  useRoutes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import "./App.css";
 import { Login } from "./components/login";
 
 const App = () => {
-  let routes = useRoutes([
-    { path: "/login", element: <Login /> },
-    
-  ]);
+  let routes = useRoutes([{ path: "/login", element: <Login /> }]);
   return routes;
 };
 export default App;
-
-
-
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,5 +1,28 @@
+import { Button, TextField } from "@mui/material";
 import React from "react";
+import "./Login.css";
 
 export const Login = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <div className="login-main">
+      <div className="text">
+        <TextField
+          id="standard-basic"
+          label="Login"
+          variant="standard"
+          color="success"
+        />
+      </div>
+      <div className="text">
+        <TextField
+          id="standard-basic"
+          label="Password"
+          variant="standard"
+          color="success"
+        />
+      </div>
+
+      <Button variant="contained">Login</Button>
+    </div>
+  );
 };
